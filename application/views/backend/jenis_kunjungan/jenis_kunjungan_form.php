12a551635d0e2aeb9f3faac1da33c2f90e3a2366
Fahrudin Yuniwinanto <fahrudinyewe@gmail.com>
2023-06-09 21:45:35 +0700

    Update jenis_kunjungan_form.php

--- application/views/backend/jenis_kunjungan/jenis_kunjungan_form.php
+++ application/views/backend/jenis_kunjungan/jenis_kunjungan_form.php
@@ -6,7 +6,7 @@
                                         echo $this->uri->segment('2');
                                     } ?></li>
 </ol>
-<h1 class="page-header">Jenis_kunjungan  <small></small></h1>
+<h1 class="page-header">Jenis Kunjungan  <small></small></h1>
 
 <?php if ($this->session->userdata('message') != '') { ?>
     <div class="alert alert-danger alert-dismissible fade show">
@@ -17,10 +17,10 @@
     <?php } ?>
 
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title" style="margin-top:0px"><?php echo $button ?> Jenis_kunjungan </h2>
+                    <h2 class="panel-title" style="margin-top:0px"><?php echo $button ?> Jenis Kunjungan </h2>
                     <div class="panel-heading-btn">
                     <a href="javascript:;" class="btn btn-xs btn-icon btn-default" data-toggle="panel-expand"><i class="fa fa-expand"></i></a>
                     <a href="javascript:;" class="btn btn-xs btn-icon btn-success" data-toggle="panel-reload"><i class="fa fa-redo"></i></a>
@@ -32,16 +32,22 @@
         <form action="<?php echo $action; ?>" method="post">
         <div class="panel-body">
 	    <div class="mb-3">
-            <label class="form-label" for="varchar">Nama Kunker <?php echo form_error('nama_kunker') ?></label>
-            <input type="text" class="form-control" name="nama_kunker" id="nama_kunker" placeholder="Nama Kunker" value="<?php echo $nama_kunker; ?>" />
+            <label class="form-label" for="varchar">Nama Jenis Kunjungan <?php echo form_error('nama_jenis_kunjungan') ?></label>
+            <input type="text" class="form-control text-uppercase" maxlength="50" name="nama_kunker" id="nama_kunker" placeholder="Nama kunker" value="<?php echo $nama_kunker; ?>" />
         </div>
 	    <div class="mb-3">
             <label class="form-label" for="int">Maksimal Kunjungan <?php echo form_error('maksimal_kunjungan') ?></label>
-            <input type="text" class="form-control" name="maksimal_kunjungan" id="maksimal_kunjungan" placeholder="Maksimal Kunjungan" value="<?php echo $maksimal_kunjungan; ?>" />
+            <div class="input-group">
+                <input type="text" class="form-control numeric" maxlength="2" name="maksimal_kunjungan" id="maksimal_kunjungan" placeholder="Maksimal Kunjungan" value="<?php echo $maksimal_kunjungan; ?>" />
+                <span class="btn btn-info">pertahun</span>
+            </div>
         </div>
 	    <div class="mb-3">
             <label class="form-label" for="int">Jumlah Hari <?php echo form_error('jumlah_hari') ?></label>
-            <input type="text" class="form-control" name="jumlah_hari" id="jumlah_hari" placeholder="Jumlah Hari" value="<?php echo $jumlah_hari; ?>" />
+            <div class="input-group">
+                <input type="text" class="form-control numeric" maxlength="2" name="jumlah_hari" id="jumlah_hari" placeholder="Jumlah Hari" value="<?php echo $jumlah_hari; ?>" />
+                <span class="btn btn-info">perkunjungan</span>
+        </div>
         </div>
 	    <input type="hidden" name="id_jenis_kunjungan" value="<?php echo $id_jenis_kunjungan; ?>" /> 
 	    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>" >
