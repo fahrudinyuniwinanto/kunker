752568994ea5f84ae55e605e79b2ade357ee63ef
Fahrudin Yuniwinanto <fahrudinyewe@gmail.com>
2023-06-18 22:36:35 +0700

    kunker list on dashboard

--- application/controllers/Backend.php
+++ application/controllers/Backend.php
@@ -9,6 +9,7 @@ class Backend extends CI_Controller
         is_logged();
         $this->load->model('Users_model');
         $this->load->model('Sy_menu_model');
+        $this->load->model('Kunker_model');
 
         $this->db->query("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
     }
@@ -19,8 +20,9 @@ class Backend extends CI_Controller
         $data = array(
 
             'content' => 'backend/dashboard',
+            'kunker_data' => $this->Kunker_model->get_limit_data(20, 0, ""),
+            'start'=>0
         );
-
 
         $this->load->view(layout(), $data);
     }
--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -64,3 +64,107 @@
     </div>
 
 </div>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="panel-title"><b>Data Permohonan Kunjungan Kerja Terbaru</b></h2>
+                <div class="panel-heading-btn">
+                    <a href="javascript:;" class="btn btn-xs btn-icon btn-default" data-toggle="panel-expand"><i class="fa fa-expand"></i></a>
+                    <a href="javascript:;" class="btn btn-xs btn-icon btn-success" data-toggle="panel-reload"><i class="fa fa-redo"></i></a>
+                    <a href="javascript:;" class="btn btn-xs btn-icon btn-warning" data-toggle="panel-collapse"><i class="fa fa-minus"></i></a>
+                    <!-- <a href="javascript:;" class="btn btn-xs btn-icon btn-danger" data-toggle="panel-remove"><i class="fa fa-times"></i></a> -->
+                </div>
+            </div>
+            <div class="panel-body">
+                <div class="row hide" style="margin-bottom: 10px">
+                    <div class="col-md-8">
+                        <?php echo anchor(site_url('kunker/create'), 'Tambah Permohonan', 'class="btn btn-flat btn-success"'); ?>
+                    </div>
+
+
+                    <div class="col-md-1 text-right">
+                    </div>
+                    <div class="col-md-3 text-right">
+                        <form action="<?php echo site_url('kunker/index'); ?>" class="form-inline" method="get">
+                            <div class="input-group">
+                                <input type="text" class="form-control" name="q" value="<?php echo @$q; ?>">
+                                <?php
+                                if (@$q <> '') {
+                                    ?>
+                                    <a href="<?php echo site_url('kunker'); ?>" class="btn btn-flat btn-default">Reset</a>
+                                <?php
+                                }
+                                ?>
+                                <button class="btn btn-flat btn-success" type="submit">Cari</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover table-condensed" style="margin-bottom: 10px">
+                        <thead class="thead-light">
+                            <tr>
+                                <th class="text-center">No</th>
+                                <th class="text-center">Jenis Kunjungan</th>
+                                <th class="text-center">Fraksi</th>
+                                <th class="text-center">Nama Anggota</th>
+                                <th class="text-center">Tingkat Keamanan</th>
+                                <!-- <th class="text-center">Nomor Surat</th> -->
+                                <!-- <th class="text-center">Tanggal Surat</th>
+                                <th class="text-center">Perihal Surat</th>
+                                <th class="text-center">Lampiran Surat</th> -->
+                                <!-- <th class="text-center">Tujuan</th> -->
+                                <th class="text-center">Tgl. Dibuat</th>
+                                <th class="text-center">Status</th>
+                                <th class="text-center">Keterangan Disposisi</th>
+                            </tr>
+                        </thead>
+                        <tbody><?php
+                                foreach ($kunker_data as $kunker) {
+                                    ?>
+                                <tr class="<?=$kunker->status_disposisi=='1'?'bg-success':($kunker->status_disposisi=='2'?'bg-danger':'')?>">
+                                    <td width="80px"><?php echo ++$start ?></td>
+                                    <td><?php echo $kunker->nama_kunker ?></td>
+                                    <td><strong><?php echo $kunker->nama_fraksi ?></strong></td>
+                                    <td><strong><?php echo $kunker->fullname ?></strong></td>
+                                    <td><?php echo $kunker->tingkat_keamanan ?></td>
+                                    <!-- <td><?php echo $kunker->nomor_surat ?></td>
+                                    <td><?php echo @$kunker->tanggal_surat ?></td>
+                                    <td><?php echo @$kunker->perihal_surat ?></td> -->
+                                    <!-- <td><?php echo $kunker->nama_daerah_tujuan ?></td> -->
+                                    <td><?php echo @$kunker->created_at ?></td>
+                                    <td class="text-center"><?php
+                                                                if ($kunker->status_disposisi == 0) {
+                                                                    echo '<label class="badge bg-warning">PENDING</label>';
+                                                                }
+                                                                if ($kunker->status_disposisi == 1) {
+                                                                    echo '<label class="badge bg-success">DISETUJUI</label>';
+                                                                }
+                                                                if ($kunker->status_disposisi == 2) {
+                                                                    echo '<label class="badge bg-danger">DITOLAK</label>';
+                                                                }
+                                                                ?></td>
+                                                                <td><?=@$diposisi_note?></td>
+                                 
+                                </tr>
+
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row hide">
+                    <div class="col-md-6">
+                        <a href="#" class="btn btn-flat btn-success">Total Record : <?php echo @$total_rows ?></a>
+                        <?php echo anchor(site_url('kunker/excel'), 'Excel', 'class="btn btn-flat btn-success"'); ?>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
